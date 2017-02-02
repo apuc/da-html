64a3508c74d5a8114b665f9fd53e1932ac3d131f
AKD47 <van83horn@gmail.com>
2017-02-02 16:25:09 +0300

    section ads resp

--- app/js/script.js
+++ app/js/script.js
@@ -1,86 +1,86 @@
 $(document).ready(function () {
-  $(".home-content__wrap_slider").owlCarousel({
-    loop: true,
-    margin: 30,
-    nav : true,
-    navText: true,
-    navigation:true,
-    /*navigationText: true,*/
-    pagination : true,
-    items: 1,
-    dots: true,
-    singleItem:false,
-    responsiveClass:true,
-    responsive: {
-      0: {
-        items: 1,
-        nav: true,
-      },
-      600: {
-        items: 1,
-        nav: true,
-      },
-      1000: {
-        items: 1,
-        nav: true,
+    $(".home-content__wrap_slider").owlCarousel({
         loop: true,
-      },
-      1200: {
-        items: 1,
+        margin: 30,
         nav: true,
-        loop: true,
+        navText: true,
+        navigation: true,
+        /*navigationText: true,*/
+        pagination: true,
+        items: 1,
+        dots: true,
+        singleItem: false,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true,
+            },
+            600: {
+                items: 1,
+                nav: true,
+            },
+            1000: {
+                items: 1,
+                nav: true,
+                loop: true,
+            },
+            1200: {
+                items: 1,
+                nav: true,
+                loop: true,
 
-      }
-    }
-  });
-  /*show ads block on mobile*/
-  $(document).on('click', '.ads__trigger', function () {
-    event.preventDefault();
-    var box = $(this).next('.ads__box'),
-        link = $(this).parent().find('.more');
-        
-    if(box.hasClass('open')){
-      box.removeClass('open').slideUp('fast');
-      link.css({display: "none"});
-    } else{
-      box.addClass('open').slideDown('fast');
-      link.css({display: "block"});
-    }
-    
-  });
-  /*close ads block on mobile*/
-  
-  /*show photo block on mobile*/
-  $(document).on('click', '.photo__trigger', function (e) {
-    event.preventDefault();
-    var box = $('.photo__box');
-        box.slideToggle('slow');
-  });
-  /*close photo block on mobile*/
-
-  /*send message modal*/
-  $(document).on('click', '.footer__send', function () {
-    event.preventDefault();
-    $('#overlay').fadeIn(400,
-        function () {
-          $('.modal-send').css('display', 'block').animate({opacity: 1}, 200);
-        });
-  });
-  $(document).on('click','.modal-send__close, #overlay',function () {
-    $('.modal-send').animate({opacity: 0}, 200,
-        function () {
-          $(this).css('display', 'none');
-          $('#overlay').fadeOut(400);
+            }
         }
-    );
-  });
+    });
+    /*show ads block on mobile*/
+    $(document).on('click', '.ads__trigger', function () {
+        event.preventDefault();
+        var box = $(this).next('.ads__box'),
+            link = $(this).parent().find('.more');
 
-  /*send message modal validation*/
-  var validMassage = new Validation();
-      validMassage.init({
-      classItem: 'valid',
-      eventElement: '#send-message-submit'
-  });
-  /*close send message modal validation*/
-  /*close send message modal*/
+        if (box.hasClass('open')) {
+            box.removeClass('open').slideUp('fast');
+            link.css({display: "none"});
+        } else {
+            box.addClass('open').slideDown('fast');
+            link.css({display: "block"});
+        }
+
+    });
+    /*close ads block on mobile*/
+
+    /*show photo block on mobile*/
+    $(document).on('click', '.photo__trigger', function (e) {
+        event.preventDefault();
+        var box = $('.photo__box');
+        box.slideToggle('slow');
+    });
+    /*close photo block on mobile*/
+
+    /*send message modal*/
+    $(document).on('click', '.footer__send', function () {
+        event.preventDefault();
+        $('#overlay').fadeIn(400,
+            function () {
+                $('.modal-send').css('display', 'block').animate({opacity: 1}, 200);
+            });
+    });
+    $(document).on('click', '.modal-send__close, #overlay', function () {
+        $('.modal-send').animate({opacity: 0}, 200,
+            function () {
+                $(this).css('display', 'none');
+                $('#overlay').fadeOut(400);
+            }
+        );
+    });
+
+    /*send message modal validation*/
+    var validMassage = new Validation();
+    validMassage.init({
+        classItem: 'valid',
+        eventElement: '#send-message-submit'
+    });
+    /*close send message modal validation*/
+    /*close send message modal*/
 });
