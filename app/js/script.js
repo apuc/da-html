fb7687be1e4015f3e549bc53e8984e0e06bff5a3
Sibi7 <Sibi_7@mail.ru>
2018-01-15 14:08:55 +0300

    fix bug

--- app/js/script.js
+++ app/js/script.js
@@ -1568,18 +1568,9 @@ $(document).ready(function () {
     /*=================================================================
     / folding post parsing page
     ==================================================================*/
-    // var parserClose = document.querySelector('.single-parser-element');
-    // $(document).on('click', '.parser__close', function () {
-    //     $('.single-parser-element').slideToggle('slow');
-    //     if ($(parserClose).css =("display","none")){
-    //         $('.parser__close').html('Раскрыть элемент');
-    //     }
-    //     return false;
-    // });
-
     $(document).on('click', '.parser__close', function () {
         $('.single-parser-element').slideToggle('slow');
-        $('.parser__close').toggleClass('change')
+        $('.parser__close').toggleClass('change');
         $('.parser__close').text(function(i, text) {
             return text === 'закрыть элемент' ? 'раскрыть элемент' : 'закрыть элемент';
         });
