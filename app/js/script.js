c808ad637ede0e6491cdf01ba2b06df8462295d2
Sibi7 <Sibi_7@mail.ru>
2018-04-02 04:28:20 +0300

    add Reviews for single-shop

--- app/js/script.js
+++ app/js/script.js
@@ -1892,7 +1892,7 @@ $(document).ready(function () {
    ==================================================================*/
 
     //Ретинг звездочек на странице одного товара
-    $('#stars li').on('mouseover', function () {
+    $('.stars li').on('mouseover', function () {
         var onStar = parseInt($(this).data('value'), 10);
 
         $(this).parent().children('li.star').each(function (e) {
@@ -1909,7 +1909,7 @@ $(document).ready(function () {
             $(this).removeClass('hover');
         });
     });
-    $('#stars li').on('click', function () {
+    $('.stars li').on('click', function () {
         var onStar = parseInt($(this).data('value'), 10);
         var stars = $(this).parent().children('li.star');
         for (i = 0; i < stars.length; i++) {
@@ -1918,7 +1918,7 @@ $(document).ready(function () {
         for (i = 0; i < onStar; i++) {
             $(stars[i]).addClass('selected');
         }
-        var ratingValue = parseInt($('#stars li.selected').last().data('value'), 10);
+        var ratingValue = parseInt($('.stars li.selected').last().data('value'), 10);
         var msg = "";
         if (ratingValue > 1) {
             msg = "Thanks! You rated this " + ratingValue + " stars.";
